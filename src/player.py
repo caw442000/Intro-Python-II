4ccd1be2ae2be0b9376151c33aae6b0ef6e67786
cedric-winbush <cawinbushjr@gmail.com>
2020-06-04 15:42:57 -0500

    adds ability to add inventory to player

--- src/player.py
+++ src/player.py
@@ -1,9 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-    def __init__(self, name, current_room):
+    def __init__(self, name, current_room, inventory = None):
         self.name = name
         self.current_room = current_room
+        self.inventory = [] if not inventory else inventory
 
     def __str__(self):
         print(self.name, self.current_room)
